db067fc3006df934893f55f8e70f784a932c56e7
Otavio Carmo <otavio.carmo2019@outlook.com>
2023-06-05 15:44:59 -0300

    feat: Creating hero page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between w-full p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-center w-full p-24">
+      <section className="pt-8 pb-6">
+        <h1 className="my-4 inline-block font-bold text-yellow-600 text-3xl">Hello Visitor 👋</h1>
+        <p className="my-2 text-xl">I'm Otávio and this is my website. Here you can see my recent works, blog posts and contributions.</p>
+      </section>
+    </main>
   );
 }
