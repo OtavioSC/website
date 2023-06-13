8f0d41143af68a4957dbd6f66e00fdbc89590872
Otavio Carmo <otavio.carmo2019@outlook.com>
2023-06-13 11:55:07 -0300

    feat: added work and about pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center w-full p-24">
+      <section className={`text-white pt-8 pb-6`}>
+        <h1 className="my-4 inline-block text-yellow-600 text-4xl">
+          About page 👋
+        </h1>
+        <p className="my-2 text-xl"></p>
+      </section>
+    </main>
+  );
+}
---
+++ src/app/work/page.tsx
@@ -0,0 +1,12 @@
+export default function Work() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center w-full p-24">
+      <section className={`text-white pt-8 pb-6`}>
+        <h1 className="my-4 inline-block text-yellow-600 text-4xl">
+          Work page 👋
+        </h1>
+        <p className="my-2 text-xl"></p>
+      </section>
+    </main>
+  );
+}
