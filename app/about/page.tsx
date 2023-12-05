eef1bedd52b8140607b6b14bdeda13530f7f47c6
Otávio Carmo <otavio.carmo2019@Outlook.com>
2023-12-05 09:15:55 -0300

    feat: first files

---
+++ app/about/page.tsx
@@ -0,0 +1,27 @@
+import { Authors, allAuthors } from 'contentlayer/generated'
+import { MDXLayoutRenderer } from 'pliny/mdx-components'
+import AuthorLayout from '@/layouts/AuthorLayout'
+import { coreContent } from 'pliny/utils/contentlayer'
+import { genPageMetadata } from 'app/seo'
+
+export const metadata = genPageMetadata({ title: 'About' })
+
+export default function Page() {
+  const author = allAuthors.find((p) => p.slug === 'default') as Authors
+  const mainContent = coreContent(author)
+
+  return (
+    <>
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          About
+        </h1>
+      </div>
+      <div>
+        <AuthorLayout content={mainContent} description={true}>
+          <MDXLayoutRenderer code={author.body.code} />
+        </AuthorLayout>
+      </div>
+    </>
+  )
+}
---
+++ data/projectsData.ts
@@ -0,0 +1,10 @@
+const projectsData = [
+  {
+    title: 'Learn4Elixir',
+    description: `Open source project to teach Elixir for developers`,
+    imgSrc: '/static/images/learn4elixir.png',
+    href: 'https://github.com/Universidade-Livre/Learn4Elixir',
+  },
+]
+
+export default projectsData
